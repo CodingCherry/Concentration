6c1a21d7f076fce4c4e817ccafd8512f8583d1fc
CodingCherry <cherry.alexeeva@gmail.com>
2018-10-08 20:53:13 +0700

    Add cards and flip functionality

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -9,12 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCard(withEmoji: "👻", on: sender)
     }
+    
+    
+    @IBAction func touchSecondCard(_ sender: UIButton) {
+        flipCard(withEmoji: "🎃", on: sender)
+    }
+    
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        
+        if button.currentTitle == emoji {
+            button.setTitle("", for: .normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5649327099, blue: 0.1909349545, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: .normal)
+            button.backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
+        }
+        
+    }
+    
 
-
+    
+    
 }
+
+
 
